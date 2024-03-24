83b73e90b323b11406b1e98edbd32e65c8d7efa4
Wellington Calixto <wac.monti@outlook.com>
2024-03-24 16:32:57 -0300

    Exclusão do componente searchinput e criação das chamadas para api

---
+++ src/api/Api.jsx
@@ -0,0 +1,116 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:4000/graphql",
+  timeout: 2000,
+  headers: {
+    "content-type": "application/json",
+  },
+});
+
+async function getAllCategories() {
+  let responseObject;
+  let errorResponse;
+  const dataQuery = {
+    query: `{getAllCategories {
+      id
+      name
+    }}`,
+  };
+  try {
+    const response = await api.post(``, dataQuery);
+    if (response.status !== 200) throw new Error(response);
+    responseObject = response;
+  } catch (error) {
+    // error is handled in catch block
+    if (error.response) {
+      // status code out of the range of 2xx
+      console.log("Data :", error.response);
+      console.log("Status :" + error.response.status);
+    } else if (error.request) {
+      // The request was made but no response was received
+      console.log(error.request);
+    } else {
+      // Error on setting up the request
+      console.log("Error", error.message);
+    }
+    errorResponse = error;
+  }
+  return { responseObject, errorResponse };
+}
+
+async function getFoodListByCategoryId(id) {
+  let responseObject;
+  let errorResponse;
+  const dataQuery = {
+    query: `{
+      getCategoryById(id: ${id}) {
+      foods {
+        id
+        name
+      }
+    }}`,
+  };
+  try {
+    const response = await api.post(``, dataQuery);
+    if (response.status !== 200) throw new Error(response);
+    responseObject = response;
+  } catch (error) {
+    // error is handled in catch block
+    if (error.response) {
+      // status code out of the range of 2xx
+      console.log("Data :", error.response);
+      console.log("Status :" + error.response.status);
+    } else if (error.request) {
+      // The request was made but no response was received
+      console.log(error.request);
+    } else {
+      // Error on setting up the request
+      console.log("Error", error.message);
+    }
+    errorResponse = error;
+  }
+  return { responseObject, errorResponse };
+}
+
+async function getFoodDataById(id) {
+  let responseObject;
+  let errorResponse;
+  const dataQuery = {
+    query: `{
+      getFoodById(id: ${id}) {
+        id
+        name
+        nutrients {
+          carbohydrates
+          protein
+          sodium
+          dietaryFiber
+          kcal
+          kJ
+        }
+      }}`,
+  };
+  try {
+    const response = await api.post(``, dataQuery);
+    if (response.status !== 200) throw new Error(response);
+    responseObject = response;
+  } catch (error) {
+    // error is handled in catch block
+    if (error.response) {
+      // status code out of the range of 2xx
+      console.log("Data :", error.response);
+      console.log("Status :" + error.response.status);
+    } else if (error.request) {
+      // The request was made but no response was received
+      console.log(error.request);
+    } else {
+      // Error on setting up the request
+      console.log("Error", error.message);
+    }
+    errorResponse = error;
+  }
+  return { responseObject, errorResponse };
+}
+
+export default { getAllCategories, getFoodListByCategoryId, getFoodDataById };
--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,111 +1,162 @@
 import React from "react";
 import style from "./Search.module.css";
-import SearchInput from "../../components/SearchInput";
-import useSearchInput from "../../custom_hooks/useSearchInput";
 
+import api from "../../api/Api";
+import Select from "react-select";
 const Search = () => {
-  const categoria = useSearchInput();
-  const [comida, setComida] = React.useState({});
-  const [categorias, setCategorias] = React.useState([
-    {
-      value: "categoria 1",
-      id: 1,
-    },
-    {
-      value: "categoria 2",
-      id: 2,
-    },
-    {
-      value: "categoria 3",
-      id: 3,
-    },
-    {
-      value: "categoria 4",
-      id: 4,
-    },
-    {
-      value: "categoria 5",
-      id: 5,
-    },
-    {
-      value: "categoria 6",
-      id: 6,
-    },
-    {
-      value: "categoria 7",
-      id: 7,
-    },
-    {
-      value: "categoria 8",
-      id: 8,
-    },
-    {
-      value: "categoria 9",
-      id: 9,
-    },
-    {
-      value: "categoria 10",
-      id: 10,
-    },
-  ]);
+  const [categoriasOptions, setCategoriasOptions] = React.useState([]);
+  const [foodsOptions, setFoodsOptions] = React.useState([]);
+  const [foodSelected, setFoodSelected] = React.useState();
+  const [foodData, setFoodData] = React.useState();
+  const [loading, setLoading] = React.useState(true);
 
+  React.useEffect(() => {
+    async function getAllCategories() {
+      setLoading(true);
+      const { responseObject } = await api.getAllCategories();
+      const categorias = responseObject.data.data.getAllCategories;
+      const map = categorias.map((categoria) => {
+        return {
+          value: categoria.id,
+          label: categoria.name,
+        };
+      });
+      setCategoriasOptions(map);
+      setLoading(false);
+    }
+    getAllCategories();
+  }, []);
+
+  async function getFoodByCategory(categoriId) {
+    const { responseObject } = await api.getFoodListByCategoryId(categoriId);
+    const foods = responseObject.data.data.getCategoryById.foods;
+
+    const map = foods.map((categoria) => {
+      return {
+        value: categoria.id,
+        label: categoria.name,
+      };
+    });
+
+    setFoodsOptions(map);
+  }
+
+  function handleChangeCategoria(choice) {
+    getFoodByCategory(choice.value);
+  }
+  function handleChangeFood(choice) {
+    setFoodSelected(choice.value);
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    console.log(foodSelected);
+
+    const { responseObject } = await api.getFoodDataById(foodSelected);
+    const foodData = responseObject.data.data.getFoodById;
+    setFoodData(foodData);
+  }
+
+  if (loading) return <h1>Carregando...</h1>;
   return (
     <section className={style.searchSection}>
       <h1 className={`${style.searchTitle} display-1`}>Pesquisar</h1>
       <h2 className={`${style.searchSubTitle} heading-3`}>
         Encontre os dados de determido alimimento
       </h2>
-      <form action="">
-        <SearchInput
-          options={categorias}
-          searchValue={categoria.value}
-          setSearchValue={categoria.setValue}
-          error={categoria.error}
-          setError={categoria.setError}
-          name={"categoria"}
-          placeholder="Por favor selecione uma categoria"
-        />
-        <button>Pesquisar</button>
+      <form className={style.searchForm} onSubmit={(e) => handleSubmit(e)}>
+        <div
+          className={`${style.searchSelectContainer} ${style.searchSelectCategoria}`}
+        >
+          <label
+            htmlFor="categoriaSelect"
+            className={`${style.searchSelectLabel} label-1`}
+          >
+            Categorias
+          </label>
+          <Select
+            className={`${style.searchSelect} label-1`}
+            id="categoriaSelect"
+            name="categoriaSelect"
+            placeholder="Selecione uma categoria"
+            options={categoriasOptions}
+            onChange={(choice) => handleChangeCategoria(choice)}
+          />
+        </div>
+        <div
+          className={`${style.searchSelectContainer} ${style.searchSelectFood}`}
+        >
+          <label
+            htmlFor="foodSelect"
+            className={`${style.searchSelectLabel} label-1`}
+          >{`Food's`}</label>
+          <Select
+            className={`${style.searchSelect} label-1`}
+            id="foodSelect"
+            name="foodSelect"
+            options={foodsOptions}
+            placeholder="Selecione uma comida"
+            onChange={(choice) => handleChangeFood(choice)}
+            isDisabled={foodsOptions.length === 0 ? true : false}
+          />
+        </div>
+        <button
+          className={`${style.searchFormButton} cta-medium`}
+          type="submit"
+          disabled={!foodSelected ? true : false}
+        >
+          Pesquisar
+        </button>
       </form>
-      {/* <table>
-        <tr>
-          <th className={`${style.searchTableTitle} label-1`} colSpan={"2"}>
-            Melancia
-          </th>
-        </tr>
-        <tr>
-          <th></th>
-          <th className={`${style.data} label-1`}>100 g</th>
-        </tr>
-        <tr>
-          <td className="label-1">Valor Energético (Kcal)</td>
-          <td className={`${style.data} label-1`}>130</td>
-        </tr>
-        <tr>
-          <td className="label-1">Carboidratos (g)</td>
-          <td className={`${style.data} label-1`}>130</td>
-        </tr>
-        <tr>
-          <td className="label-1">Proteínas (g)</td>
-          <td className={`${style.data} label-1`}>130</td>
-        </tr>
-        <tr>
-          <td className="label-1">Gordutas Totais (g)</td>
-          <td className={`${style.data} label-1`}>130</td>
-        </tr>
-        <tr>
-          <td className="label-1">Fibras alimentares (g)</td>
-          <td className={`${style.data} label-1`}>130</td>
-        </tr>
-        <tr>
-          <td className="label-1">Sódio (mg)</td>
-          <td className={`${style.data} label-1`}>130</td>
-        </tr>
-      </table> */}
-      <p className="label-1">
-        Obs: Todos os dados são referente a quarta edição da tabela TACO (Tabela
-        Brasileira de Composição de Alimentos)
-      </p>
+      {foodData && (
+        <>
+          <table>
+            <tr>
+              <th className={`${style.searchTableTitle} label-1`} colSpan={"2"}>
+                {foodData.name}
+              </th>
+            </tr>
+            <tr>
+              <th></th>
+              <th className={`${style.data} label-1`}>100 g</th>
+            </tr>
+            <tr>
+              <td className="label-1">Valor Energético (Kcal)</td>
+              <td className={`${style.data} label-1`}>
+                {foodData.nutrients.kcal || "0"}
+              </td>
+            </tr>
+            <tr>
+              <td className="label-1">Carboidratos (g)</td>
+              <td className={`${style.data} label-1`}>
+                {foodData.nutrients.carbohydrates || "0"}
+              </td>
+            </tr>
+            <tr>
+              <td className="label-1">Proteínas (g)</td>
+              <td className={`${style.data} label-1`}>
+                {foodData.nutrients.protein || "0"}
+              </td>
+            </tr>
+            <tr>
+              <td className="label-1">Fibras alimentares (g)</td>
+              <td className={`${style.data} label-1`}>
+                {foodData.nutrients.dietaryFiber || "0"}
+              </td>
+            </tr>
+            <tr>
+              <td className="label-1">Sódio (mg)</td>
+              <td className={`${style.data} label-1`}>
+                {foodData.nutrients.sodium || "0"}
+              </td>
+            </tr>
+          </table>
+          <p className="label-1">
+            Obs: Todos os dados são referente a quarta edição da tabela TACO
+            (Tabela Brasileira de Composição de Alimentos)
+          </p>
+        </>
+      )}
     </section>
   );
 };
