573eb18061e9685503028e00b5a330149725619e
Wellington Calixto <wac.monti@outlook.com>
2024-04-04 21:22:32 -0300

    Estilizanção do componente header

--- src/components/header/HeaderComponent.jsx
+++ src/components/header/HeaderComponent.jsx
@@ -1,13 +1,35 @@
 import React from "react";
 import style from "./HeaderComponent.module.css";
+import { NavLink } from "react-router-dom";
 const HeaderComponent = () => {
   return (
     <header className={style.header}>
-      <h1 className={`${style.headerTitle} heading-2`}>CN</h1>
       <nav>
         <ul>
-          <li className="cta-medium">Pesquiasar</li>
-          <li className="cta-medium">Comparar</li>
+          <li className={style.headerNavLinkStart}>
+            <NavLink className={`${style.headerNavLink} `} to="/">
+              <h1 className={`${style.headerTitle} heading-2`}>CN</h1>
+            </NavLink>
+          </li>
+          <li className={style.headerNavLinkEnd}>
+            <NavLink
+              className={`${style.headerNavLink} ${style.headerNavLinkHover}`}
+              to="/search"
+            >
+              <p className="cta-medium">Pesquisar</p>
+            </NavLink>
+          </li>
+          <li>
+            <p> | </p>
+          </li>
+          <li className={style.headerNavLinkEnd}>
+            <NavLink
+              className={`${style.headerNavLink} ${style.headerNavLinkHover}`}
+              to="/comparison"
+            >
+              <p className="cta-medium"> Comparar</p>
+            </NavLink>
+          </li>
         </ul>
       </nav>
     </header>
