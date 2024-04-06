7d6ae4852adb3b9a6be22f3e1bca470675f7c45a
Wellington Calixto <wac.monti@outlook.com>
2024-04-06 17:41:36 -0300

    Criação da tela de comparison e ajustes no footer

--- src/pages/comparison/Comparison.jsx
+++ src/pages/comparison/Comparison.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import style from "./Comparison.module.css";
 import ComparisonSvg from "../../assets/comparison-icon.svg?react";
+import OptionsSvg from "../../assets/options.svg?react";
 
 import api from "../../api/Api";
 import Select from "react-select";
+import FooterComponent from "../../components/footer/FooterComponent";
+import TableComponent from "../../components/table/TableComponent";
+import HeaderComponent from "../../components/header/HeaderComponent";
 
 const Comparison = () => {
   const [foodsOptions, setFoodsOptions] = React.useState([]);
@@ -48,155 +52,83 @@ const Comparison = () => {
 
   if (loading) return <h1>Carregando...</h1>;
   return (
-    <section className={style.comparisonSection}>
-      <h1 className={`${style.comparisonTitle} display-1`}>Comparação</h1>
-      <h2 className={`${style.comparisonSubTitle} heading-3`}>
-        Compare dois alimentos.
-      </h2>
-      <form className={style.comparisonForm} onSubmit={(e) => handleSubmit(e)}>
-        <div
-          className={`${style.comparisonSelectContainer} ${style.comparisonSelectCategoria}`}
+    <>
+      <HeaderComponent />
+      <section className={style.comparisonSection}>
+        <OptionsSvg className={style.comparisonSvgOption} />
+        <h2 className={`${style.comparisonSubTitle} heading-3`}>
+          Compare os dados nutricionais de 2 alimentos para ver qual se encaixa
+          melhor na sua dieta.
+        </h2>
+        <form
+          className={style.comparisonForm}
+          onSubmit={(e) => handleSubmit(e)}
         >
-          <label
-            htmlFor="categoriaSelect"
-            className={`${style.comparisonSelectLabel} label-1`}
+          <div
+            className={`${style.comparisonSelectContainer} ${style.comparisonSelectCategoria}`}
           >
-            Food 1
-          </label>
-          <Select
-            className={`${style.comparisonSelect} label-1`}
-            id="categoriaSelect"
-            name="categoriaSelect"
-            placeholder="Selecione uma categoria"
-            options={foodsOptions}
-            onChange={(choice) => setFood1Selected(choice.value)}
-          />
-        </div>
-        <div
-          className={`${style.comparisonSelectContainer} ${style.comparisonSelectFood}`}
-        >
-          <label
-            htmlFor="foodSelect"
-            className={`${style.comparisonSelectLabel} label-1`}
+            <label
+              htmlFor="categoriaSelect"
+              className={`${style.comparisonSelectLabel} label-1`}
+            >
+              Food 1
+            </label>
+            <Select
+              id="categoriaSelect"
+              name="categoriaSelect"
+              placeholder="Selecione uma categoria"
+              options={foodsOptions}
+              onChange={(choice) => setFood1Selected(choice.value)}
+            />
+          </div>
+          <div
+            className={`${style.comparisonSelectContainer} ${style.comparisonSelectFood}`}
           >
-            Food 2
-          </label>
-          <Select
-            className={`${style.comparisonSelect} label-1`}
-            id="foodSelect"
-            name="foodSelect"
-            options={foodsOptions}
-            placeholder="Selecione uma comida"
-            onChange={(choice) => setFood2Selected(choice.value)}
-            isDisabled={foodsOptions.length === 0 ? true : false}
-          />
-        </div>
-        <button
-          className={`${style.comparisonFormButton} cta-medium`}
-          type="submit"
-          disabled={!food1Selected && !food2Selected ? true : false}
-        >
-          Pesquisar
-        </button>
-      </form>
-      {foodData1 && foodData2 && (
-        <div className={style.comparisonTableContainer}>
-          <table>
-            <tr>
-              <th
-                className={`${style.comparisonTableTitle} label-1`}
-                colSpan={"2"}
-              >
-                {foodData1.name}
-              </th>
-            </tr>
-            <tr>
-              <th></th>
-              <th className={`${style.data} label-1`}>100 g</th>
-            </tr>
-            <tr>
-              <td className="label-1">Valor Energético (Kcal)</td>
-              <td className={`${style.data} label-1`}>
-                {foodData1.nutrients.kcal || "0"}
-              </td>
-            </tr>
-            <tr>
-              <td className="label-1">Carboidratos (g)</td>
-              <td className={`${style.data} label-1`}>
-                {foodData1.nutrients.carbohydrates || "0"}
-              </td>
-            </tr>
-            <tr>
-              <td className="label-1">Proteínas (g)</td>
-              <td className={`${style.data} label-1`}>
-                {foodData1.nutrients.protein || "0"}
-              </td>
-            </tr>
-            <tr>
-              <td className="label-1">Fibras alimentares (g)</td>
-              <td className={`${style.data} label-1`}>
-                {foodData1.nutrients.dietaryFiber || "0"}
-              </td>
-            </tr>
-            <tr>
-              <td className="label-1">Sódio (mg)</td>
-              <td className={`${style.data} label-1`}>
-                {foodData1.nutrients.sodium || "0"}
-              </td>
-            </tr>
-          </table>
-          <ComparisonSvg className={style.comparisonIcon} />
-          <table>
-            <tr>
-              <th
-                className={`${style.comparisonTableTitle} label-1`}
-                colSpan={"2"}
-              >
-                {foodData2.name}
-              </th>
-            </tr>
-            <tr>
-              <th></th>
-              <th className={`${style.data} label-1`}>100 g</th>
-            </tr>
-            <tr>
-              <td className="label-1">Valor Energético (Kcal)</td>
-              <td className={`${style.data} label-1`}>
-                {foodData2.nutrients.kcal || "0"}
-              </td>
-            </tr>
-            <tr>
-              <td className="label-1">Carboidratos (g)</td>
-              <td className={`${style.data} label-1`}>
-                {foodData2.nutrients.carbohydrates || "0"}
-              </td>
-            </tr>
-            <tr>
-              <td className="label-1">Proteínas (g)</td>
-              <td className={`${style.data} label-1`}>
-                {foodData2.nutrients.protein || "0"}
-              </td>
-            </tr>
-            <tr>
-              <td className="label-1">Fibras alimentares (g)</td>
-              <td className={`${style.data} label-1`}>
-                {foodData2.nutrients.dietaryFiber || "0"}
-              </td>
-            </tr>
-            <tr>
-              <td className="label-1">Sódio (mg)</td>
-              <td className={`${style.data} label-1`}>
-                {foodData2.nutrients.sodium || "0"}
-              </td>
-            </tr>
-          </table>
-          <p className="label-1">
-            Obs: Todos os dados são referente a quarta edição da tabela TACO
-            (Tabela Brasileira de Composição de Alimentos)
-          </p>
-        </div>
-      )}
-    </section>
+            <label
+              htmlFor="foodSelect"
+              className={`${style.comparisonSelectLabel} label-1`}
+            >
+              Food 2
+            </label>
+            <Select
+              id="foodSelect"
+              name="foodSelect"
+              options={foodsOptions}
+              placeholder="Selecione uma comida"
+              onChange={(choice) => setFood2Selected(choice.value)}
+            />
+          </div>
+          <button
+            className={`${style.comparisonFormButton} cta-medium`}
+            type="submit"
+            disabled={!food1Selected && !food2Selected ? true : false}
+          >
+            Comparar
+          </button>
+        </form>
+        {foodData1 && foodData2 && (
+          <>
+            <span className={style.comparisonDiviser}></span>
+            <div className={style.comparisonResultContainer}>
+              <TableComponent
+                data={foodData1}
+                className={style.comparisonTable1}
+              />
+              <ComparisonSvg className={style.comparisonSvgIcon} />
+              <TableComponent
+                data={foodData2}
+                className={style.comparisonTable2}
+              />
+              <p className={`${style.resultMessage} label-1`}>
+                Obs: Todos os dados são referente a quarta edição da tabela TACO
+                (Tabela Brasileira de Composição de Alimentos)
+              </p>
+            </div>
+          </>
+        )}
+      </section>
+      <FooterComponent />
+    </>
   );
 };
 
